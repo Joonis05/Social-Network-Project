205d6f643ee06ed406b38e33276e9bf68603c556
Joonis <fabiaguilar0205@gmail.com>
2024-04-24 00:19:41 -0500

    create class UserModel

---
+++ Project/backend/logic_bussines.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime, date 
+
+
+class UserModel(BaseModel):
+
+    id : int
+    name : str
+    username : str
+    email : EmailStr
+    password : str
+    biography : str
+    birthday: date
+    profile_picture: str
+    created_at : datetime
+
+
