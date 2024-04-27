ced9dba5c7c428296c9ce9b1e58d19939183b5d8
Joonis <fabiaguilar0205@gmail.com>
2024-04-26 23:55:03 -0500

    updating user table attributes

--- Project/backend/db_connection.py
+++ Project/backend/db_connection.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-from logic_bussines import UserModel
 
 engine = create_engine("postgresql://joonis:0205@localhost/db_joonis")
 Session = sessionmaker(engine)
@@ -18,14 +17,17 @@ class User(Base):
     id : Mapped[int]  = mapped_column(Integer(), primary_key=True)
     name : Mapped[str] = mapped_column(String(50), nullable=False)
     username : Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    birthday : Mapped[str] = mapped_column(DateTime.date)
     email : Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password : Mapped[str] = mapped_column(String(50), nullable=False)
-    biography: Mapped[str] = mapped_column(String)
-    profile_picture : Mapped[str] = mapped_column(String)
-    created_at = mapped_column(DateTime(), default=datetime.now)
+    biography: Mapped[str] = mapped_column(String, nullable=True)
+    profile_picture : Mapped[str] = mapped_column(String, nullable=True)
+    created_at = mapped_column(DateTime(), default=datetime.utcnow, nullable=False)
+    updated_at = mapped_column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
 
     def __str__(self):
         return self.username
+
 
 if __name__ == "__main__":
     Base.metadata.drop_all(engine)
