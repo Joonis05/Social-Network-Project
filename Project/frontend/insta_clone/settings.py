d77e6c0730f0a082f7b8eb32a56cc8c591c1e2c5
Andrescpyo <cerdaspadillaandres@gmail.com>
2024-04-28 22:08:02 -0500

    test #1

--- Project/frontend/insta_clone/settings.py
+++ Project/frontend/insta_clone/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     #own
     'posts',
+    #'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL =  '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+LOGIN_REDIRECT_URL = ''
+LOGOUT_REDIRECT_URL = ''
+LOGIN_URL = ''
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
