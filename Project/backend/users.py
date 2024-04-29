9c4fb75bc0086244db8b1e6365ccc20164e6c7e4
Joonis05 <91146858+Joonis05@users.noreply.github.com>
2024-04-29 18:07:36 +0000

    create "create_user" method for "UserModel" class

--- Project/backend/users.py
+++ Project/backend/users.py
@@ -1,5 +1,13 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime, date
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from models import User 
+import hashlib
+
+engine = create_engine("")
+Session = Session(engine)
+
 
 
 class UserModel(BaseModel):
@@ -13,4 +21,24 @@ class UserModel(BaseModel):
     birthday: date
     profile_picture: str
     created_at : datetime
+
+    @classmethod
+    def create_user(name, username, email, password, birthday, profile_picture=None, biography=None, ):
+
+        password_hash = hashlib.sha256(password)
+        new_user = User(
+            name = name,
+            username = username,
+            email = email,
+            password = password_hash,
+            biography = biography,
+            birthday = birthday,
+            profile_picture = profile_picture
+        )
+
+        Session.add(new_user)
+        Session.commit()
+        Session.close()
+        
+
 
