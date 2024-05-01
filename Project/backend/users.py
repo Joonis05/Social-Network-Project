f14633ce053d3168bdb85396efb87abf5afc2907
Joonis05 <91146858+Joonis05@users.noreply.github.com>
2024-04-30 23:35:20 -0500

    update the "create_user" function

--- Project/backend/users.py
+++ Project/backend/users.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime, date
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-from models import User 
+from database import User 
 import hashlib
 
 engine = create_engine("")
@@ -20,25 +20,36 @@ class UserModel(BaseModel):
     biography : str
     birthday: date
     profile_picture: str
-    created_at : datetime
+    created_at : datetime.utcnow
 
-    @classmethod
-    def create_user(name, username, email, password, birthday, profile_picture=None, biography=None, ):
+class UserLogin(BaseModel):
+    username : str
+    password : str
 
-        password_hash = hashlib.sha256(password)
-        new_user = User(
-            name = name,
-            username = username,
-            email = email,
-            password = password_hash,
-            biography = biography,
-            birthday = birthday,
-            profile_picture = profile_picture
+def create_user(user : UserModel):
+    if len(user.name) < 3 or len(user.username) > 64:
+        raise ValueError("The name must be between 3 and 64 characters")
+    if len(user.username) < 3 or len(user.username) > 64:
+        raise ValueError("The username must be between 3 and 64 characters")
+    if not user.password:
+        raise ValueError("The password is required")
+    if user.birthday > datetime.utcnow():
+        raise ValueError("The birthday must be in the past")
+
+    password_hash = hashlib.sha256(user.password)
+    new_user = User(
+        name = user.name,
+        username = user.username,
+        email = user.email,
+        password = password_hash,
+        biography = user.biography,
+        birthday = user.birthday,
+        profile_picture = user.profile_picture
         )
 
-        Session.add(new_user)
-        Session.commit()
-        Session.close()
+    Session.add(new_user)
+    Session.commit()
+    return new_user
         
 
 
