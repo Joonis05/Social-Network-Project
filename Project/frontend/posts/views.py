93e6d694f5a7e7bfbfd2e2e507b41288c87b7512
Andrescpyo <cerdaspadillaandres@gmail.com>
2024-06-08 19:22:49 -0500

    new 'accounts' app and template integration

--- Project/frontend/posts/views.py
+++ Project/frontend/posts/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def posts(request):
-    return HttpResponse("Hello, World!")
+    return render(request, 'posts/posts.html')
