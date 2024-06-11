5e700de766d2c814bf91dda57c77b58864a4eda4
Joonis05 <fabiaguilar0205@gmail.com>
2024-06-11 15:30:15 -0500

    update database.py

--- Project/backend/database.py
+++ Project/backend/database.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from sqlalchemy import create_engine
 
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
+engine = create_engine("postgresql://fabian:0205@localhost:5432/postgres")
 
 class Base(DeclarativeBase):
     pass
@@ -25,14 +25,17 @@ class User(Base):
     updated_at = mapped_column(DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)
     followers : Mapped[List["Followers"]] = relationship(back_populates="user")
     following : Mapped[List["Following"]] = relationship(back_populates="user")
+    posts : Mapped[List["Post"]] = relationship(back_populates="user")
 
 class Followers(Base):
     __tablename__ = "followers"
-    followers_id : Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
+    user_id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    follower_id : Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
     user : Mapped["User"] = relationship(back_populates="followers")
 
 class Following(Base):
     __tablename__ = "following"
+    user_id : Mapped[int] = mapped_column(Integer, primary_key=True)
     following_id : Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
     user : Mapped["User"] = relationship(back_populates="following")
 
@@ -42,12 +45,10 @@ class Post(Base):
     id : Mapped[int] = mapped_column(Integer, primary_key=True)
     content : Mapped[str] = mapped_column(String, nullable=False)
     created_at : Mapped[str] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
-    updated_at : Mapped[str] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
     user_id : Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
-    
-    user = Relationship("User", back_populates="posts")
+    user : Mapped["User"] = relationship(back_populates="posts")
 
-class Comments(Base):
+"""class Comments(Base):
     __tablename__ = "comments"
 
     id : Mapped[int] = mapped_column(Integer, primary_key=True)
@@ -59,7 +60,7 @@ class Comments(Base):
     
     
     user = Relationship("User", back_populates="comments")
-    post = Relationship("Post", back_populates="comments")
+    post = Relationship("Post", back_populates="comments")"""
 
 if __name__ == "__main__":
 
