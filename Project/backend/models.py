cd0a23c69ff00a58056f42054715f14c9acb91d4
Joonis05 <fabiaguilar0205@gmail.com>
2024-06-11 18:41:28 -0500

    Docstrings

--- Project/backend/models.py
+++ Project/backend/models.py
@@ -1,28 +1,77 @@
 from pydantic import BaseModel
 
+
 class UserModel(BaseModel):
+    """
+    This class represents a pydantic model for a user.
+
+    Attributes:
+        name (str): The user's name.
+        username (str): The user's username.
+        email (str): The user's email address.
+        password (str): The user's password.
+        biography (str): The user's biography.
+        profile_picture (str): The user's profile picture.
+    """
+
     name: str
     username: str
     email: str
     password: str
     biography: str = None
     profile_picture: str = None
-    
+
+
 class UserCredentials(BaseModel):
+    """
+    This class represents a pydantic model for user credentials.
+
+    Attributes:
+        username (str): The user's username.
+        password (str): The user's password.
+    """
+
     username: str
     password: str
 
+
 class FollowModel(BaseModel):
+    """
+    This class represents a pydantic model for a follow relationship.
+
+    Attributes:
+        follower (str): The user who is following the user.
+        followed (str): The user who is being followed.
+    """
+
     follower: str
     followed: str
 
+
 class PostModel(BaseModel):
-    id : int = None
-    content : str
-    username : str
+    """
+    This class represents a pydantic model for a post.
+
+    Attributes:
+        content (str): The content of the post.
+        username (str): The username of the user who made the post.
+    """
+
+    id: int = None
+    content: str
+    username: str
+
 
 class CommentModel(BaseModel):
-    id : int = None
-    content : str
-    username : str
-    post_id : int 
+    """
+    This class represents a pydantic model for a comment.
+
+    Attributes:
+        content (str): The content of the comment.
+        username (str): The username of the user who made the comment.
+    """
+
+    id: int = None
+    content: str
+    username: str
+    post_id: int
