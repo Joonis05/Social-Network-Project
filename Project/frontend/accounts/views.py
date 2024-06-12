6c5f46cc1f51c4e7e326001bb4b90b3283d4d28f
Andrescpyo <cerdaspadillaandres@gmail.com>
2024-06-12 09:29:50 -0500

    register

--- Project/frontend/accounts/views.py
+++ Project/frontend/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -6,10 +6,13 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data['username']
             messages.success(request, f"User {username} created!")
+            return redirect('accounts:login')
     else:
         form = UserCreationForm()
+        messages.success(request, f"User {username} not created!")
 
     return render(request, 'accounts/register.html', {'form': form})
 
