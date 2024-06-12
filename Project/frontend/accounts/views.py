f8d1f4e71c3c1986a4db842f9972454b07b03eeb
Andrescpyo <cerdaspadillaandres@gmail.com>
2024-06-12 04:31:13 -0500

    final commit

--- Project/frontend/accounts/views.py
+++ Project/frontend/accounts/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            messages.success(request, f"User {username} created!")
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
 
 def login_user(request):
     return render(request, 'accounts/login.html')
--- Project/frontend/posts/views.py
+++ Project/frontend/posts/views.py
@@ -7,6 +7,25 @@ def posts(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/posts.html', {"posts": posts})
 
+def comments(request, pk):
+    post = Post.objects.get(id=pk)
+    comments = Comment.objects.filter(post=post).order_by('-created_at')
+    return render(request, 'posts/comments.html', {"post": post, "comments": comments})
+
+def create_comment(request, pk):
+    post = Post.objects.get(id=pk)
+    form = CommentForm()
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect("posts:comments", pk=post.id)
+
+    return render(request, 'posts/create_comment.html', {"form": form, "post": post})
 
 def create_post(request):
     form = PostForm()
