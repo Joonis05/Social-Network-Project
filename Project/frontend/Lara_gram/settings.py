5ebf38f2120996fd60a009e68dd738339c1ce615
Andrescpyo <cerdaspadillaandres@gmail.com>
2024-06-12 02:35:12 -0500

    render data2

--- Project/frontend/Lara_gram/settings.py
+++ Project/frontend/Lara_gram/settings.py
@@ -122,6 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Add your static files directory here
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
--- Project/frontend/posts/urls.py
+++ Project/frontend/posts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.posts),
+    path('', views.posts, name="posts"),
+    path('create-post/', views.create_post, name="create-post"),
+    #str:pk is the primary key of the post
+    path('edit-post/<str:pk>/', views.edit_post, name="edit-post"),
+    path('delete-post/<str:pk>/', views.delete_post, name="delete-post"),
 ]
